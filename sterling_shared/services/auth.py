e55403a00257b94770c1c1ec3c8b728ed461c134
Daniel Ale <danielale9291@gmail.com>
2023-05-04 08:25:55 +0100

    Initialize ft/ridwanray/customer-management-permissions

---
+++ sterling_shared/services/auth.py
@@ -0,0 +1,23 @@
+import os
+
+from sterling_shared.services.base import BaseRequest
+
+
+class AuthService(BaseRequest):
+    base_url = f"{os.getenv('AUTH_SERVICE_URL', 'http://localhost:10050')}/api/v1"
+
+    def get_users_by_ids(self, ids: list, to_dict: bool = False):
+        string_ids = ",".join(ids)
+        path = f'users/fetch/by-id/?id={string_ids}'
+        users = self.send_request("GET", path)
+        if to_dict:
+            return {x["id"]: x for x in users['data']}
+        return users['data']
+
+    def get_users_by_permissions(self, permissions: list, to_dict: bool = False):
+        string_permissions = ",".join(permissions)
+        path = f'users/fetch/by-permission/?permissions={string_permissions}'
+        users = self.send_request("GET", path)
+        if to_dict:
+            return {x["id"]: x for x in users['data']}
+        return users['data']
