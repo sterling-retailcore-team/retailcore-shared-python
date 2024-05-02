011491db4be2e413a0d9d8ff9ffe8e32521762e3
phourxx <48600936+phourxx@users.noreply.github.com>
2024-05-02 15:29:12 +0100

    Update auth.py

--- sterling_shared/services/auth.py
+++ sterling_shared/services/auth.py
@@ -4,7 +4,16 @@ from sterling_shared.services.base import BaseRequest
 
 
 class AuthService(BaseRequest):
-    base_url = f"{os.getenv('AUTH_SERVICE_URL', 'http://localhost:10050')}/api/v1"
+    base_url = None
+
+    def __init__(self, request):
+        super().__init__(request)
+        base_url = f"{os.getenv('AUTH_SERVICE_URL', 'http://localhost:10050')}/api/v1"
+        self.base_url = f"{base_url}"
+
+    def get_user_by_id(self, id):
+        path = f'users/{id}'
+        return self.send_request("GET", path)
 
     def get_users_by_ids(self, ids: list, to_dict: bool = False):
         string_ids = ",".join(ids)
