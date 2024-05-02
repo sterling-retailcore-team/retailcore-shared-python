7afa720b9ec2fab81242ebdd375e125f24dcbcec
Folakemi <folakemi@bepeerless.co>
2024-05-02 11:17:49 +0100

    date serialization and format

--- sterling_shared/auditlog/utils.py
+++ sterling_shared/auditlog/utils.py
@@ -29,6 +29,8 @@ def create_log(endpoint_name, token_key, meta, user_request, action_type, action
     decoded_token = jwt.decode(token_key, options={"verify_signature": False})
     role_ids = ", ".join(str(role_id) for role_id in decoded_token.get("role_ids", ""))
     role_names = ", ".join(str(role_name) for role_name in decoded_token.get("role_names", ""))
+    fullname = f"{user_request.firstname} {user_request.lastname}"
+    
     log_data = {
     "action": action,
     "sourceIP": meta.get('source_ip', 'Unknown'),
@@ -41,22 +43,21 @@ def create_log(endpoint_name, token_key, meta, user_request, action_type, action
     "newValuesJson": new_value_json,
     "affectedColumns": affected_columns,
     "role": role_names,
-    "userName": getattr(user_request, 'username', ''),
-    "fullName": f"{getattr(user_request, 'firstname', '')} {getattr(user_request, 'lastname', '')}",
+    "userName": user_request.username if user_request.username else "",
+    "fullName": fullname if fullname else "",
     "userID": str(getattr(user_request, 'id', '')),
     "createdDate": user_request.created_at.strftime("%Y-%m-%d %H:%M:%S") if isinstance(user_request.created_at, datetime.datetime) else "",
     "ipAddress": meta.get('ip_address', 'Unknown'),
     "startDate": user_request.created_at.strftime("%Y-%m-%d %H:%M:%S") if isinstance(user_request.created_at, datetime.datetime) else "",
     "endDate": user_request.created_at.strftime("%Y-%m-%d %H:%M:%S") if isinstance(user_request.created_at, datetime.datetime) else "",
-    "branchCode": getattr(user_request, 'branch_code', ''),
+    "branchCode": str(user_request.branch_code) if user_request.branch_code else "",
     "location": meta.get('ip_address', 'Unknown'),
-    "branchName": getattr(user_request, 'branch', ''),
+    "branchName": user_request.branch if user_request.branch else "",
     "clientInfo": extract_browser_name(meta.get('user_agent', '')),
     "actionStatus": extract_browser_name(meta.get('user_agent', '')),
     "lastLogin": user_request.last_login.strftime("%Y-%m-%d %H:%M:%S") if isinstance(user_request.last_login, datetime.datetime) else "",
     "sessionID": microservice_name,
     "module": module,
-    "fullname": f"{getattr(user_request, 'firstname', '')} {getattr(user_request, 'lastname', '')}",
     "moduleID": module_id,
     "timestamp": user_request.created_at.strftime("%Y-%m-%d %H:%M:%S") if isinstance(user_request.created_at, datetime.datetime) else "",
     }
