21e0e8a52f3bb351a4eaa09ff695fabf9b069b66
phourxx <48600936+phourxx@users.noreply.github.com>
2024-06-10 08:55:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sterling-utils",
-    version="0.1.5",
+    version="0.1.6",
     author="Folayemi Bello",
     author_email="fola@bepeerless.co",
     packages=setuptools.find_packages(),
