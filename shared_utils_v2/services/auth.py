c49f33529b72e4534f9bce4e7e9468c5b4b2dcb3
jaysinscars <jaysinscars@gmail.com>
2025-04-22 23:03:18 +0100

    feat: revert to no more service base path

--- shared_utils_v2/services/auth.py
+++ shared_utils_v2/services/auth.py
@@ -9,16 +9,17 @@ class AuthService(BaseRequest):
         super().__init__(request)
         base_url = f"{os.getenv('AUTH_SERVICE_URL', 'http://localhost:10050')}/api/v1"
         self.base_url = f"{base_url}"
-        self.base_path = os.getenv('AUTH_SERVICE_BASE_PATH',
-                                   'service')
+        self.base_path = ''
+        # self.base_path = os.getenv('AUTH_SERVICE_BASE_PATH',
+        #                            'service')
 
     def get_user_by_id(self, id):
-        path = f'user/{self.base_path}/{id}'
+        path = f'users/{id}'
         return self.send_request("GET", path)
 
     def get_users_by_ids(self, ids: list, to_dict: bool = False):
         string_ids = ",".join(ids)
-        path = f'user/{self.base_path}/fetch/by-id?id={string_ids}'
+        path = f'users/fetch/by-id?id={string_ids}'
         users = self.send_request("GET", path)
         if to_dict:
             return {x["id"]: x for x in users['data']}
@@ -26,7 +27,7 @@ class AuthService(BaseRequest):
 
     def get_users_by_permissions(self, permissions: list, to_dict: bool = False):
         string_permissions = ",".join(permissions)
-        path = f'user/{self.base_path}/fetch/by-permission?permissions={string_permissions}'
+        path = f'users/fetch/by-permission?permissions={string_permissions}'
         users = self.send_request("GET", path)
         if to_dict:
             return {x["id"]: x for x in users['data']}
